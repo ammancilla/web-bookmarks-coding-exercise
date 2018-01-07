38a4931b202d8b19593b37210135c9b97e9765fb
Alfonso Mancilla <almancill@gmail.com>
2018-01-07 20:52:28 +0100

    Add missing :title and :shortening fields to bookmarks

--- db/migrate/20180107182215_create_bookmarks.rb
+++ db/migrate/20180107182215_create_bookmarks.rb
@@ -3,6 +3,8 @@ class CreateBookmarks < ActiveRecord::Migration[5.1]
     create_table :bookmarks do |t|
       t.references :site, foreign_key: true, index: true, null: false
       t.text :url, null: false
+      t.string :title, null: false
+      t.string :shortening
 
       t.timestamps
     end
