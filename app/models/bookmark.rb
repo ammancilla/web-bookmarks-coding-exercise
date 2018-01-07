1498ed2b9d613682d532fc682b6648e64985bf91
Alfonso Mancilla <almancill@gmail.com>
2018-01-07 20:52:47 +0100

    add validations for :title and :shortening attributes

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,4 +4,8 @@ class Bookmark < ApplicationRecord
 
   # Associations
   belongs_to :site, counter_cache: true
+
+  # Validations
+  validates :title, presence: true
+  validates :shortening, format: { with: URI.regexp( %w(http https) ) }, allow_blank: true
 end
