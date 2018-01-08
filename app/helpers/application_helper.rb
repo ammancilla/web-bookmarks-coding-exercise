b93928eb9b57564946d3451591ee626ef3e3825e
Alfonso Mancilla <almancill@gmail.com>
2018-01-08 16:48:21 +0100

    add helper method to render flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def render_flash_messages
+    return if flash.empty?
+
+    flash.inject('') do |html, flash_data|
+      flash_type = flash_data[0]
+      flash_msg = flash_data[1]
+
+      alert_type =
+        case flash_type
+        when 'notice'
+          'info'
+        when 'error'
+          'danger'
+        else
+          flash_type
+        end
+
+      html += render_alert_tag(alert_type, flash_msg)
+    end.html_safe
+
+  end
+
+  def render_alert_tag(type, msg)
+    content_tag(:div, class: "alert alert-#{type}") { msg }
+  end
 end
