7aa3fe530396e4657eedb5ffcb3eff0c425fe060
Alfonso Mancilla <almancill@gmail.com>
2018-01-08 16:42:08 +0100

    Add scopes to filter bookmarks. Add Bookmark#chain_scopes to dinamically filter bookmarks chaining scopes.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -10,4 +10,22 @@ class Bookmark < ApplicationRecord
 
   # Validations
   validates :title, :url, presence: true
+
+  # Scopes
+  scope :recents, -> { order(created_at: :desc) }
+  scope :by_title, -> (title) { where("title LIKE ?", "%#{title}%") }
+  scope :by_url, -> (url) { where("url LIKE ?", "%#{url}%") }
+  scope :by_shortening, -> (shortening) { where("title LIKE ?'", "%#{shortening}%") }
+
+  # Methods
+  def self.chain_scopes(scopes)
+    return [] if scopes.blank?
+
+    scopes.inject(self) do |chain, skope|
+      scope_name = "by_#{skope[0]}"
+      scope_param = skope[1]
+
+      chain.send(scope_name, scope_param)
+    end
+  end
 end
