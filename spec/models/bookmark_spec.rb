d5e423c53259b3a469e59f451a0ea2b9ef264310
Alfonso Mancilla <almancill@gmail.com>
2018-01-09 12:48:51 +0100

    Add unit tests

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -2,5 +2,31 @@ require 'rails_helper'
 require 'models/concerns/url_attributes_spec'
 
 describe Bookmark, type: :model do
+  context 'callbacks' do
+    let!(:bookmark) { build(:bookmark, url: 'http://apo.co/oinolopa') }
+
+    describe 'before_validation: :assign_site' do
+      it "creates a Site if it doesn't exists" do
+        expect { bookmark.valid? }.to change { Site.count }.from(0).to(1)
+      end
+
+      it 'assigns the proper site' do
+        expect { bookmark.valid? }.to change { bookmark.site }.from(nil).to(Site)
+      end
+    end
+  end
+
+  describe '.chain_scopes' do
+    let!(:query) { "SELECT \"bookmarks\".* FROM \"bookmarks\" WHERE (title LIKE '%test%') AND (url LIKE '%apo.co%')" }
+
+    subject { Bookmark.chain_scopes(title: 'test', url: 'apo.co') }
+
+    it 'generates a single query' do
+      expect( subject.to_sql ).to eq(query)
+    end
+
+    it { is_expected.to be_an(ActiveRecord::Relation)  }
+  end
+
   it_behaves_like 'class with URL attributes', [:url, :shortening]
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -2,5 +2,19 @@ require 'rails_helper'
 require 'models/concerns/url_attributes_spec'
 
 describe Site, type: :model do
+  let!(:site) { create(:site, url: 'https://example.com/apolonio') }
+
+  context 'callbacks' do
+    describe 'before_save: :format_attributes' do
+      it 'assings :protocol' do
+	expect(site.protocol).to eq('https')
+      end
+
+      it 'assings :hostname' do
+	expect(site.hostname).to eq('example.com')
+      end
+    end
+  end
+
   it_behaves_like 'class with URL attributes', [:url]
 end
