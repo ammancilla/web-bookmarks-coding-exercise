6c263028ad465d294fd69f9fa1c9b91a5afb017b
Alfonso Mancilla <almancill@gmail.com>
2018-01-10 15:33:58 +0100

    Improve Site.address_from_url

--- app/models/site.rb
+++ app/models/site.rb
@@ -15,15 +15,21 @@ class Site < ApplicationRecord
 
   # Methods
   def self.address_from_url(url)
+    return if url.nil?
+
     uri = URI.parse(url)
+
+    unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+      return
+    end
+
     "#{uri.scheme}://#{uri.host}"
-  rescue URI::InvalidURIError
-    url
   end
 
   private
 
   def format_url
-    self.url = Site.address_from_url(url)
+    address = Site.address_from_url(url)
+    self.url = address unless address.nil?
   end
 end
